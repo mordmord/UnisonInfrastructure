8c6056e4eeb24c4d27204ec7de142480a9c3a0d6
Ryan Mord <mord@one20.com>
2018-01-30 10:21:25 -0600

    Build

---
+++ app/src/main/java/com/one20/unisoninfrastructureapp/activity/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.one20.unisoninfrastructureapp.activity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by ryanmord on 1/30/18.
+ */
+class BaseActivity: AppCompatActivity() {
+}
